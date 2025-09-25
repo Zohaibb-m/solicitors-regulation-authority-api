a971685a7ffb695187712473ff711e263fcf5470
Zohaib Munir <zohaibmunir32@gmail.com>
2025-09-25 22:03:47 +0500

    Added vercel main running code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.router import router
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 app.include_router(router)
@@ -8,3 +9,5 @@ app.include_router(router)
 def read_root():
     return {"message": "Welcome to the Solicitors Regulation Authority API"}
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8800)
